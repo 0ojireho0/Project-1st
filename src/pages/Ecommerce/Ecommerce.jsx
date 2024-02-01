679b27b07c4612494375dec478cba862efa3ca74
Jeremiah Quintano <jeremiahquintano16@gmail.com>
2024-02-01 18:01:55 +0800

    pagination done

--- src/pages/Ecommerce/Ecommerce.jsx
+++ src/pages/Ecommerce/Ecommerce.jsx
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AllProducts from './AllProducts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { Button, Card, CardBody, CardHeader, Typography } from '@material-tailwind/react';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { Card, CardBody, CardHeader, Typography } from '@material-tailwind/react';
 
 const Ecommerce = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [visibleProducts, setVisibleProducts] = useState(8); // Number of products to display initially
- 
+  const [visibleProducts, setVisibleProducts] = useState(8);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +18,7 @@ const Ecommerce = () => {
         setProducts(response.data);
         setLoading(false);
       } catch (error) {
-        console.log(error)
+        console.log(error);
         setLoading(false);
       }
     };
@@ -28,8 +27,29 @@ const Ecommerce = () => {
   }, []);
 
   const loadMore = () => {
-    setVisibleProducts(prevVisibleProducts => prevVisibleProducts + 8); // Increase the number of visible products
+    setLoading(true);
+    setTimeout(() => {
+      setVisibleProducts((prevVisibleProducts) => prevVisibleProducts + 8);
+      setLoading(false);
+    }, 500);
   };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+      !loading
+    ) {
+      // User has scrolled to the bottom, and not currently loading
+      loadMore();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loading, visibleProducts]);
 
   return (
     <>
@@ -43,17 +63,10 @@ const Ecommerce = () => {
         </CardHeader>
         <CardBody>
           <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-5 grid-cols-1 md:mx-10'>
-            {products.slice(0, visibleProducts).map(item => (
+            {products.slice(0, visibleProducts).map((item) => (
               <AllProducts key={item.id} item={item} />
             ))}
           </div>
-          {visibleProducts < products.length && (
-            <div className='justify-center items-center flex'>
-              <Button onClick={loadMore} className='mt-5'>
-                Load More
-              </Button>
-            </div>
-          )}
         </CardBody>
       </Card>
     </>
