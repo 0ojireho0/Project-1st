42217269e4078e63dd414e40203e1290c2765c5e
Jeremiah Quintano <jeremiahquintano16@gmail.com>
2024-02-06 16:56:12 +0800

    404 error updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import Taskgiver from './pages/Taskgiver'
+import React, { useState } from 'react';
+import Taskgiver from './pages/Taskgiver';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'
-import Ecommerce from './pages/Ecommerce/Ecommerce'
-import AddCart from './pages/Ecommerce/AddCart'
-import Error404 from './components/Error404'
-import { ShopContextProvider } from './pages/Ecommerce/ShopContext'
-
-
+import Navbar from './components/Navbar';
+import Ecommerce from './pages/Ecommerce/Ecommerce';
+import AddCart from './pages/Ecommerce/AddCart';
+import Error404 from './components/Error404';
+import { ShopContextProvider } from './pages/Ecommerce/ShopContext';
 
 const App = () => {
-  return (
-  <>
+  const [is404, setIs404] = useState(false);
 
-      <ShopContextProvider>
-        <Router>
-          <Navbar />
-            <Routes>
-              <Route exact path='/' element={<Taskgiver />} /> 
-              <Route path='/ecommerce' element={<Ecommerce />} />
-              <Route path='addcart' element={<AddCart />} />
-              <Route path='*' element={<Error404 />}/>
-            </Routes>
-        </Router>
-      </ShopContextProvider>
-  </>
-  )
+  return (
+    <ShopContextProvider>
+      <Router>
+        {!is404 && <Navbar />}
+        <Routes>
+          <Route exact path='/' element={<Taskgiver />} /> 
+          <Route path='/ecommerce' element={<Ecommerce />} />
+          <Route path='/addcart' element={<AddCart />} />
+          <Route
+            path='*'
+            element={<Error404 on404={() => setIs404(true)} />}
+          />
+        </Routes>
+      </Router>
+    </ShopContextProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
+const Error404 = ({ on404 }) => {
+  useEffect(() => {
+    on404();
+  }, [on404]);
 
-const Error404 = () => {
   return (
-    <>
-        <div>ERROR 404</div>
-    
-    </>
-  )
+    <div>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
 }
 
-export default Error404
+export default Error404;
